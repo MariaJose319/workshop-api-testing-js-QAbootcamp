c5a030e171ec0ee0ec1bbe4f211cc6549761fa0b
MariaJose319 <mjsierra319@gmail.com>
2022-06-29 17:25:08 -0500

    StaticCodeAnalysis (#5)

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -3,88 +3,87 @@ const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
 describe('First Api Tests', () => {
+  it('Consume GET', async () => {
+    const response = await axios.get('https://httpbin.org/ip');
 
-    it('Consume GET', async () => {
-        const response = await axios.get('https://httpbin.org/ip');
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property('origin');
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data).to.have.property('origin');
-    });
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-    it('Consume GET Service with query parameters', async () => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+    const response = await axios.get('https://httpbin.org/get', { query });
 
-        const response = await axios.get('https://httpbin.org/get', { query });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.query).to.eql(query);
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.query).to.eql(query);
-    });
+  // Get only headers
+  it('consume HEAD', async () => {
+    const response = await axios.head('https://httpbin.org/headers');
 
-    //Get only headers
-    it('consume HEAD', async () => {
-        const response = await axios.head('https://httpbin.org/headers');
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.headers).to.have.property('content-type', 'application/json');
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.headers).to.have.property('content-type', 'application/json');
-    });
+  // Partial updates
+  it('Consume PATCH', async () => {
+    const body = {
+      name: 'Maria',
+      lastname: 'Sierra',
+      age: '30',
+      city: 'Cartagena'
+    };
 
-    //Partial updates
-    it('Consume PATCH', async () => {
-        const body = {
-            name: 'Maria',
-            lastname: 'Sierra',
-            age: '30',
-            city: 'Cartagena'
-        };
+    const response = await axios.patch('https://httpbin.org/patch', body);
 
-        const response = await axios.patch('https://httpbin.org/patch', body);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json).to.eql(body);
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.eql(body);
-    });
+  // Replace the resource in its entirety
+  it('Consume PUT', async () => {
+    const body = {
+      name: 'Maria',
+      lastname: 'Sierra',
+      age: '30',
+      city: 'Cartagena'
+    };
 
-    //Replace the resource in its entirety
-    it('Consume PUT', async () => {
-        const body = {
-            name: 'Maria',
-            lastname: 'Sierra',
-            age: '30',
-            city: 'Cartagena'
-        };
+    const response = await axios.put('https://httpbin.org/put', body);
 
-        const response = await axios.put('https://httpbin.org/put', body);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json).to.eql(body);
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.eql(body);
-    });
+  it('Consume DELETE', async () => {
+    const body = {
+      name: 'Maria',
+      lastname: 'Sierra',
+      age: '30',
+      city: 'Cartagena'
+    };
 
-    it('Consume DELETE', async () => {
-        const body = {
-            name: 'Maria',
-            lastname: 'Sierra',
-            age: '30',
-            city: 'Cartagena'
-        };
-    
-        const response = await axios.delete('https://httpbin.org/delete', body);
-        expect(response.status).to.equal(StatusCodes.OK);
-    });
+    const response = await axios.delete('https://httpbin.org/delete', body);
+    expect(response.status).to.equal(StatusCodes.OK);
+  });
 
-    it('Consume POST', async () => {
-        const body = {
-            name: 'Maria',
-            lastname: 'Sierra',
-            age: '30',
-            city: 'Cartagena'
-        };
+  it('Consume POST', async () => {
+    const body = {
+      name: 'Maria',
+      lastname: 'Sierra',
+      age: '30',
+      city: 'Cartagena'
+    };
 
-        const response = await axios.post('https://httpbin.org/post', body);
+    const response = await axios.post('https://httpbin.org/post', body);
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.json).to.eql(body);
-    });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json).to.eql(body);
+  });
 });
