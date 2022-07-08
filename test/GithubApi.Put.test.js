177f36946250fc6e08e1eee919a59094dcd375da
MariaJose319 <mjsierra319@gmail.com>
2022-07-08 18:50:05 -0500

    PutMethod3

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -22,4 +22,24 @@ describe('Consume PUT Method', () => {
     const followingInfo = response.data.find((element) => element.login === 'aperdomob');
     assert.exists(followingInfo);
   });
+
+  it('follow aperdomob again - idempotent', async () => {
+    const response = await axios.put('https://api.github.com/user/following/aperdomob', {}, {
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    expect(response.data).to.equal('');
+  });
+
+  it('verify that I follow aperdomob - idempotent', async () => {
+    const response = await axios.get('https://api.github.com/user/following', {
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    const followingInfo = response.data.find((element) => element.login === 'aperdomob');
+    assert.exists(followingInfo);
+  });
 });
