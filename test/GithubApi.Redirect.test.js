4cd560ff8044f4e32eb37e7276c4c60770e4e727
MariaJose319 <mjsierra319@gmail.com>
2022-07-14 12:34:53 -0500

    HeadMethod#1

---
+++ test/GithubApi.Redirect.test.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+describe.only('Consume HEAD Method', () => {
+  let response;
+
+  before(async () => {
+    response = await axios.head('https://github.com/aperdomob/redirect-test');
+  });
+
+  it('should redirect the url', async () => {
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.request.res.responseUrl).to.equal('https://github.com/aperdomob/new-redirect-test');
+  });
+
+  it('consume get method and redirect the url', async () => {
+    const URLresponse = await axios.get('https://github.com/aperdomob/redirect-test');
+    expect(URLresponse.status).to.equal(StatusCodes.OK);
+  });
+});
